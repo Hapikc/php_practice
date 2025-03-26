e3ed932fd203eee0a2ca6f8128f43e201dc2a64d
Hapikc <136709525532hs@gmail.com>
2025-03-26 16:07:38 +0700

    sessions and redirects done

--- core/Src/Sessions.php
+++ core/Src/Sessions.php
@@ -2,7 +2,20 @@
 
 namespace Src;
 
-class Sessions
+class Session
 {
+    public static function set($name, $value): void
+    {
+        $_SESSION[$name] = $value;
+    }
 
+    public static function get($name)
+    {
+        return $_SESSION[$name] ?? null;
+    }
+
+    public static function clear($name)
+    {
+        unset($_SESSION[$name]);
+    }
 }
